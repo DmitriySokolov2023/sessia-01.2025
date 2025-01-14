82e12cebdcdc0be8559ab12839a0db704ee03b39
dmitriysokolov2023 <sokolovoff2023@gmail.com>
2025-01-14 21:37:53 +0300

    add-1

--- 1Orlov/cod-py/zadanie-1-3.py
+++ 1Orlov/cod-py/zadanie-1-3.py
@@ -1,20 +1,31 @@
+from random import randint  
+
 numbers = []
 for i in range(4):
-    num = int(input(f"Введите число {i + 1}: "))
-    numbers.append(num)
+    numbers.append(randint(1,5))
 
-# Проверяем наличие одинаковых чисел
-has_duplicates = False
+print(numbers)
 
-for i in range(4):
-    for j in range(i + 1, 4):
-        if numbers[i] == numbers[j]:
-            has_duplicates = True
-            break
+max = numbers[0]
+min = numbers[0]
 
+for num in numbers:
+    if num > max:
+        max = num
+    if num < min:
+        min = num
 
-# Вывод результата
-if has_duplicates:
-    print("Среди введенных чисел есть одинаковые.")
-else:
-    print("Все введенные числа различны.")
+count_max = 0
+count_min = 0
+
+#Ищем кол-во повторений
+for num in numbers:
+    if(num == max):
+        count_max+=1
+    if(num == min):
+        count_min+=1
+
+print('Максимальное число: ',max)
+print('Минимальное число: ', min)
+print('Кол-во максимальных чисел: ', count_max)
+print('Кол-во минимальных чисел: ', count_min)
